8f4f4ad7f9e89ba7284ee14f9f828c1c67879b3f
boutboutnico <boutwork@gmail.com>
2018-10-19 11:33:17 +0200

    send html email

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 import yaml
 import smtplib
@@ -19,20 +20,26 @@ def main():
         
     print(secure_data)
 
-    send_email(secure_data)
+    body= """<h3>HOHOHO,</h3>
+    <p>
+    Noël approche à grand pas et j'ai besoin de volontaires pour m'aider dans la distribution de tous les cadeaux !<br>
+    Comme l'année précédente, j'ai besoin de personnels qualifiés. C'est pourquoi tu trouveras un petit jeu te permettant de me prouver ta bravoure !<br>
+    Réponds moi au plus vite avec la phrase secrète pour être inscrit sur la liste de noël. <br>
+    Le premier, la première à répondre aura la chance de commencer le bal des cadeaux ! <br>
+    </p>
+    <h3>Le Père Noël de la Frémo</h3>"""
+    
+    send_email(secure_data, "Le Père Noël de la Frémo recrute", body , secure_data['receiver_test'])
 
-def send_email(secure_data):
+def send_email(secure_data, subject, body, toaddr):
     # http://naelshiab.com/tutoriel-comment-envoyer-un-courriel-avec-python/
     print ("sending email...")
     fromaddr = secure_data['sender_email']
-    toaddr = secure_data['receiver_test']
     msg = MIMEMultipart()
     msg['From'] = fromaddr
     msg['To'] = toaddr
-    msg['Subject'] = "SUBJECT OF THE MAIL"
-     
-    body = "YOUR MESSAGE HERE"
-    msg.attach(MIMEText(body, 'plain'))
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'html'))
      
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
