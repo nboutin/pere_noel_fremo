4474bf5b5ccbc2e54d244c5f788d062ecbd13cf9
boutboutnico <boutwork@gmail.com>
2018-10-19 10:14:18 +0200

    load yml file

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+#!/usr/bin/env python
+
+import yaml
 
 def main():
-    print ("main")
+    print ("##### Le Pere Noel de la Fremo #####")
+    
+    with open("data.yml", 'r') as stream:
+        data = yaml.safe_load(stream)
+        
+    print (data)
+    
 
 if __name__ == "__main__":
     main()
